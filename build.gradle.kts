c98f1caff2dcc1ea82da93d1718c467ff2e1f708
Tim203 <mctim203@gmail.com>
2023-05-04 13:31:33 +0200

    Move to GitHub Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import net.kyori.indra.git.IndraGitExtension
+
 plugins {
-    alias(libs.plugins.indra) apply false
+    alias(libs.plugins.indra)
+    alias(libs.plugins.indra.git)
 }
 
 allprojects {
@@ -12,6 +15,36 @@ subprojects {
         plugin("java-library")
         plugin("maven-publish")
         plugin("net.kyori.indra")
+        plugin("net.kyori.indra.git")
         plugin("net.kyori.indra.publishing")
     }
+
+    indra {
+        github("GeyserMC", "ConfigUtils") {
+            ci(true)
+        }
+        mitLicense()
+
+        javaVersions {
+            target(8)
+        }
+
+        configurePublications {
+            if (shouldAddBranchName()) {
+                version = versionWithBranchName()
+            }
+        }
+
+        publishSnapshotsTo("geysermc", "https://repo.opencollab.dev/artifactory/maven-snapshots")
+        publishReleasesTo("geysermc", "https://repo.opencollab.dev/artifactory/maven-releases")
+    }
 }
+
+fun Project.branchName(): String =
+        the<IndraGitExtension>().branchName() ?: System.getenv("BRANCH_NAME") ?: "local/dev"
+
+fun Project.shouldAddBranchName(): Boolean =
+        System.getenv("IGNORE_BRANCH")?.toBoolean() ?: (branchName() !in arrayOf("master", "local/dev"))
+
+fun Project.versionWithBranchName(): String =
+        branchName().replace(Regex("[^0-9A-Za-z-_]"), "-") + '-' + version
